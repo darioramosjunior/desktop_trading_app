0440249464fbbde7a7358f3a6f5fcaa5fc26d602
ramosd <darioramosjunior@gmail.com>
2023-10-28 14:40:28 +0800

    Fixed center alignment of each widgets in watchlist tab. Added calculate & clear buttons for each row.

--- watchlist_item.py
+++ watchlist_item.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QWidget, QHBoxLayout, QLabel, QComboBox, QLineEdit, QPushButton, QPlainTextEdit
+from PyQt6.QtWidgets import QWidget, QHBoxLayout, QLabel, QComboBox, QLineEdit, QPushButton
 
 
 class WatchlistItem(QWidget):
@@ -9,50 +9,88 @@ class WatchlistItem(QWidget):
         layout = QHBoxLayout()
         self.setLayout(layout)
 
-        coin = QLineEdit()
-        coin.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.coin = QLineEdit()
+        self.coin.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        watch_price = QLineEdit()
-        watch_price.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.watch_price = QLineEdit()
+        self.watch_price.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        trigger_condition = QComboBox()
-        trigger_condition.addItems(["        > / =      ", "          < / =        "])
+        self.trigger_condition = QComboBox()
+        self.trigger_condition.addItems(["> / =", "< / ="])
 
-        trigger_price = QLineEdit()
-        trigger_price.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.trigger_price = QLineEdit()
+        self.trigger_price.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        status = QLabel("       ALERT      ")  # TO-DO: To set dynamically
-        status.setStyleSheet("border: 0.5px solid black;")
+        self.status = QLabel("ALERT")  # TO-DO: To set dynamically
+        self.status.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.status.setStyleSheet("border: 0.5px solid black;")
 
-        port_size = QLineEdit()
-        port_size.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.port_size = QLineEdit()
+        self.port_size.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        var_percentage = QLineEdit()
-        var_percentage.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.var_percentage = QLineEdit()
+        self.var_percentage.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        cut_percentage = QLineEdit()
-        cut_percentage.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        cut_percentage.setToolTip("Percentage change between Entry & Cut in the chart")
+        self.cut_percentage = QLineEdit()
+        self.cut_percentage.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.cut_percentage.setToolTip("Percentage change between Entry & Cut in the chart")
 
-        pos_size = 8000
-        position_size = QLabel(f"        {pos_size} USD        ")
-        position_size.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        position_size.setStyleSheet("border: 1px solid black;")
+        self.position_size = QLabel("USD")
+        self.position_size.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.position_size.setStyleSheet("border: 1px solid black;")
 
         calculate_button = QPushButton("Calculate")
+        clear_button = QPushButton("Clear")
 
-        layout.addWidget(coin)
-        layout.addWidget(watch_price)
-        layout.addWidget(trigger_condition)
-        layout.addWidget(trigger_price)
-        layout.addWidget(status)
-        layout.addWidget(port_size)
-        layout.addWidget(var_percentage)
-        layout.addWidget(cut_percentage)
-        layout.addWidget(position_size)
+        # Connections
+        calculate_button.clicked.connect(self.calculate_pos_size)
+        clear_button.clicked.connect(self.clear_row)
+
+        layout.addWidget(self.coin)
+        layout.addWidget(self.watch_price)
+        layout.addWidget(self.trigger_condition)
+        layout.addWidget(self.trigger_price)
+        layout.addWidget(self.status)
+        layout.addWidget(self.port_size)
+        layout.addWidget(self.var_percentage)
+        layout.addWidget(self.cut_percentage)
+        layout.addWidget(self.position_size)
         layout.addWidget(calculate_button)
+        layout.addWidget(clear_button)
+
+        layout.setStretchFactor(self.coin, 1)
+        layout.setStretchFactor(self.watch_price, 1)
+        layout.setStretchFactor(self.trigger_condition, 1)
+        layout.setStretchFactor(self.trigger_price, 1)
+        layout.setStretchFactor(self.status, 1)
+        layout.setStretchFactor(self.port_size, 1)
+        layout.setStretchFactor(self.var_percentage, 1)
+        layout.setStretchFactor(self.cut_percentage, 1)
+        layout.setStretchFactor(self.position_size, 1)
+        layout.setStretchFactor(calculate_button, 1)
+        layout.setStretchFactor(clear_button, 1)
 
         self.show()
+
+    def calculate_pos_size(self):
+        port_size = float(self.port_size.text())
+        var_percentage = float(self.var_percentage.text())
+        var = port_size * (var_percentage / 100)
+        cut_percentage = float(self.cut_percentage.text())
+        pos_size = var / (cut_percentage / 100)
+        formatted_pos_size = "{:.2f} USD".format(pos_size)
+        self.position_size.setText(formatted_pos_size)
+
+    def clear_row(self):
+        self.coin.setText("")
+        self.watch_price.setText("")
+        self.trigger_condition.setCurrentIndex(0)
+        self.trigger_price.setText("")
+        self.port_size.setText("")
+        self.var_percentage.setText("")
+        self.cut_percentage.setText("")
+        self.position_size.setText("USD")
+        print("Clear Row")
 
 
 class Columns(QWidget):
@@ -62,27 +100,28 @@ class Columns(QWidget):
         layout = QHBoxLayout()
         self.setLayout(layout)
 
-        column1_label = QLabel("   Coin Name   ")
+        column1_label = QLabel("Coin Name")
         column1_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        column2_label = QLabel("  Watch Price  ")
+        column2_label = QLabel("Watch Price")
         column2_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        column3_label = QLabel("   Condition   ")
+        column3_label = QLabel("Condition")
         column3_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        column4_label = QLabel(" Trigger Price ")
+        column4_label = QLabel("Trigger Price")
         column4_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        column5_label = QLabel("    Status     ")
+        column5_label = QLabel("Status")
         column5_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         column6_label = QLabel("Port Size (USD)")
         column6_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        column7_label = QLabel("    VAR (%)    ")
+        column7_label = QLabel("VAR (%)")
         column7_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        column8_label = QLabel("    CUT (%)    ")
+        column8_label = QLabel("CUT (%)")
         column8_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         column8_label.setToolTip("Percentage change between Entry & Cut in the chart")
         column9_label = QLabel(" Position Size ")
         column9_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         # For column & row value alignment purpose only
-        column10_label = QLabel("               ")
+        column10_label = QLabel("")
+        column11_label = QLabel("")
 
         layout.addWidget(column1_label)
         layout.addWidget(column2_label)
@@ -94,5 +133,18 @@ class Columns(QWidget):
         layout.addWidget(column8_label)
         layout.addWidget(column9_label)
         layout.addWidget(column10_label)
+        layout.addWidget(column11_label)
+
+        layout.setStretchFactor(column1_label, 1)
+        layout.setStretchFactor(column2_label, 1)
+        layout.setStretchFactor(column3_label, 1)
+        layout.setStretchFactor(column4_label, 1)
+        layout.setStretchFactor(column5_label, 1)
+        layout.setStretchFactor(column6_label, 1)
+        layout.setStretchFactor(column7_label, 1)
+        layout.setStretchFactor(column8_label, 1)
+        layout.setStretchFactor(column9_label, 1)
+        layout.setStretchFactor(column10_label, 1)
+        layout.setStretchFactor(column11_label, 1)
 
         self.show()
